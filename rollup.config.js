892c047a9beeba24b42dd8f713c49bea4d935418
liwenyue5 <liwenyue5@jd.com>
2022-04-20 14:25:56 +0800

    feat: 修改配置

--- rollup.config.js
+++ rollup.config.js
@@ -1,39 +1,58 @@
 import glob from 'glob'
 import path from 'path'
-import resolve from 'rollup-plugin-node-resolve'
+import nodeResolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import typescript from 'rollup-plugin-typescript2'
 import json from 'rollup-plugin-json'
-import getBabelOutputPlugin from 'rollup-plugin-babel'
+import babel from 'rollup-plugin-babel'
 import { terser } from 'rollup-plugin-terser'
-import gzipPlugin from 'rollup-plugin-gzip'
 import rimraf from 'rimraf'
 import summary from 'rollup-plugin-summary'
+import nodePolyfills from 'rollup-plugin-node-polyfills'
 const rootPath = path.resolve(__dirname, './')
 const pkg = require('./package.json')
 
 rimraf(path.resolve(rootPath, 'lib/'), (err) => {})
 rimraf(path.resolve(rootPath, 'dist/'), (err) => {})
 
+const extensions = ['.js', '.ts']
+
+const getBabelOptions = ({ useESModules }) => ({
+  exclude: 'node_modules/**',
+  runtimeHelpers: true,
+  include: 'src/**',
+  extensions,
+  plugins: [['@babel/transform-runtime', { regenerator: false, useESModules }]]
+})
+
+const getEntry = () => {
+  const files = glob.sync('src/*/*.ts')
+  return files
+}
+
 function createConfig({ output, mulEntry }) {
   return {
-    input: mulEntry ? glob.sync('src/*/*.ts') : 'src/index.ts',
+    input: mulEntry ? getEntry() : 'src/index.ts',
     output,
     plugins: [
       json(),
-      resolve(),
+      nodePolyfills(),
+      nodeResolve({
+        extensions,
+        preferBuiltins: true,
+        mainFields: ['jsnext:main'],
+        browser: true,
+        modulesOnly: true
+      }),
       commonjs({
         transformMixedEsModules: true
       }),
-      getBabelOutputPlugin({
-        configFile: path.resolve(__dirname, '.babelrc')
-      }),
       typescript(),
+      babel(getBabelOptions({ useESModules: false })),
       terser(),
-      gzipPlugin(),
       summary()
     ],
-    external: [] // 不需要打入包内的第三方npm包,例如['lodash']
+    external: ['es6-promise'] // 不需要打入包内的第三方npm包,例如['lodash']
   }
 }
 
